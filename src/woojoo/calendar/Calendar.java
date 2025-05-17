70ced5f7df2e9e76e4217c497e572992c303c2dc
woojoo <woozoo0926@naver.com>
2025-05-17 20:32:49 +0900

    Add repeat function

--- src/woojoo/calendar/Calendar.java
+++ src/woojoo/calendar/Calendar.java
@@ -7,7 +7,7 @@ public class Calendar {
 	private static final int[] MAX_DAYS = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 	public int getMaxDaysOfMonth(int month) {
 		return MAX_DAYS[month - 1];
-	}
+	} 
 	
 	public void printSampleCalendar() {
 		System.out.println("일   월   화   수   목   금   토");
@@ -22,12 +22,17 @@ public class Calendar {
 		// 숫자를 입력받아 해당하는 달의 최대 일수 출력
 		Scanner scanner = new Scanner(System.in);
 		Calendar cal = new Calendar();
-		System.out.println("달을 입력하세요");
-		int month = scanner.nextInt();
 		
-		int[] maxDays = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-		System.out.printf("%d월은 %d일까지 있습니다.\n", month, cal.getMaxDaysOfMonth(month));
+		System.out.println("반복 횟수 입력하세요.");
+		int repeat = scanner.nextInt();
+		
+		for(int i=0; i<repeat; i++) {
+			System.out.println("달을 입력하세요");
+			int month = scanner.nextInt();			
+			System.out.printf("%d월은 %d일까지 있습니다.\n", month, cal.getMaxDaysOfMonth(month));
+		}
 		cal.printSampleCalendar();
+		System.out.println("Bye~");
 		scanner.close();
 	}
 }
